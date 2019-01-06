38c50925e48895cea5fee6d2e5d9e96d4643fcbe
AditiWakhale0202 <aditi_wakhale@persistent.com>
2019-01-06 14:24:10 +0530

    improvement in APIs

--- index.py
+++ index.py
@@ -1,16 +1,32 @@
 import flask
 from flask import render_template
-from flask import redirect,flash,request
+from flask import redirect, request
+from login_check import is_login
 app = flask.Flask(__name__)
 
 
-def load():
-    #flash("Hello People")
-    return render_template('index.html')
+def login():
+    return render_template('login.html')
 
 
-def create():
-    return render_template('create_warehouse.html')
+def insert_data():
+    return render_template('insertdata_page.html')
+
+
+def verify_login():
+    if request.method == 'GET':
+        return "You need to login first"
+    user = request.form['u_name']
+    password = request.form['pwd']
+    import pymongo, json
+    client = pymongo.MongoClient()
+    db = client.mydb
+    collection = db.merchant
+    for post in collection.find({'uname': user}):
+        if password == post['password']:
+            return render_template('index.html', uname=user)
+    else:
+        render_template('login.html')
 
 
 def put_data():
@@ -19,8 +35,7 @@ def put_data():
     brand = request.form['p_brand']
     category = request.form['p_cat']
     price = request.form['p_price']
-    print price
-    import pymongo, json
+    import pymongo,json
     client = pymongo.MongoClient()
     db = client.mydb
     collection = db.warehouse
@@ -44,10 +59,12 @@ def github():
     return redirect('https://alexwohlbruck.github.io/cat-facts/')
 
 
-app.add_url_rule('/my_app/load/', 'load', load)
+app.add_url_rule('/my_app/home/', 'login', login)
+app.add_url_rule('/my_app/verify', 'verify', verify_login, methods=['GET','POST'])
 app.add_url_rule('/my_app/show', 'show', show)
-app.add_url_rule('/my_app/create', 'create', create, methods=['POST'])
+app.add_url_rule('/my_app/insert', 'insert', insert_data, methods=['POST'])
 app.add_url_rule('/my_app/put_data', 'put_data', put_data, methods=['POST'])
+#  keeping it as first example
 app.add_url_rule('/my_app/github', 'github', github)
 
 if __name__ == '__main__':
